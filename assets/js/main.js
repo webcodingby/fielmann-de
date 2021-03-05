9d70ca4d48e20483fb006526f87bf3b533e11660
webcoding.by <webcoding.by@gmail.com>
2021-03-05 16:45:46 +0300

    init

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+// let link = document.querySelectorAll('a');
+
+// link.forEach(item => {
+//   itemAttr = item.getAttribute('href');
+//   itemAttr = '/';
+//   item.setAttribute('href', itemAttr)
+//   console.log(item);
+// })
